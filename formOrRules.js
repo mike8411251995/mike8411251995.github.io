30c3569a045dd4d10d27d0a9694cec26a188f904
Mike Lee <mike8411251995@yahoo.com.tw>
2021-09-10 14:45:45 +0800

    demo done

--- formOrRules.js
+++ formOrRules.js
@@ -60,9 +60,6 @@
             { key: 'group00node00', img: 'group00node00', group: '0' },
             { key: 'group00node01', img: 'group00node01', group: '0' },
             { key: 'group00node02', img: 'group00node02', group: '0' },
-            { key: 'group00node03', img: 'group00node03', group: '0' },
-            { key: 'group00node04', img: 'group00node04', group: '0' },
-            { key: 'group00node05', img: 'group00node05', group: '0' },
             { key: '1', isGroup: true },
             { key: 'group01node00', img: 'group01node00', group: '1' },
             { key: 'group01node01', img: 'group01node01', group: '1' },
@@ -70,23 +67,33 @@
             { key: 'group01node03', img: 'group01node03', group: '1' },
             { key: 'group01node04', img: 'group01node04', group: '1' },
             { key: 'group01node05', img: 'group01node05', group: '1' },
+            { key: 'group01node06', img: 'group01node06', group: '1' },
+            { key: 'group01node07', img: 'group01node07', group: '1' },
+            { key: 'group01node08', img: 'group01node08', group: '1' },
+            { key: 'group01node09', img: 'group01node09', group: '1' },
+            { key: '2', isGroup: true },
+            { key: 'group02node00', img: 'group02node00', group: '2' },
+            { key: 'group02node01', img: 'group02node01', group: '2' },
         ], [
             { from: 'group00node00', to: 'group00node01', img: 'link00' },
-            { from: 'group00node01', to: 'group00node02', img: 'link01' },
-            { from: 'group00node02', to: 'group00node03', img: 'link02' },
-            { from: 'group00node03', to: 'group00node04', img: 'link03' },
-            { from: 'group00node04', to: 'group01node00', img: 'link04' },
-            { from: 'group01node00', to: 'group01node01', img: 'link05' },
-            { from: 'group01node01', to: 'group01node02', img: 'link06' },
-            { from: 'group01node02', to: 'group01node03', img: 'link07' },
-            { from: 'group01node03', to: 'group01node04', img: 'link08' },
-            { from: 'group01node04', to: 'group01node05', img: 'link09' },
-            { from: 'group01node05', to: 'group00node05', img: 'link10' },
+            { from: 'group00node01', to: 'group02node00', img: 'link01' },
+            { from: 'group02node00', to: 'group02node01', img: 'link02' },
+            { from: 'group02node01', to: 'group01node00', img: 'link03' },
+            { from: 'group01node00', to: 'group01node01', img: 'link04' },
+            { from: 'group01node01', to: 'group01node02', img: 'link05' },
+            { from: 'group01node02', to: 'group01node03', img: 'link06' },
+            { from: 'group01node03', to: 'group01node04', img: 'link07' },
+            { from: 'group01node04', to: 'group01node05', img: 'link08' },
+            { from: 'group01node05', to: 'group01node06', img: 'link09' },
+            { from: 'group01node06', to: 'group01node07', img: 'link10' },
+            { from: 'group01node07', to: 'group01node08', img: 'link11' },
+            { from: 'group01node08', to: 'group01node09', img: 'link12' },
+            { from: 'group01node09', to: 'group00node02', img: 'link13' },
         ]);
     }
 
     function convertKeyImage(img) {
-        return "imgs/" + img + ".png";
+        return "imgs/formOrRules/" + img + ".png";
     }
 
     exports.init = init;
--- formPhmRules.js
+++ formPhmRules.js
@@ -4,14 +4,14 @@
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "../../gojs/release/go.js"], factory);
+        define(["require", "exports", "./gojs/release/go.js"], factory);
     }
 })(function (require, exports) {
     'use strict';
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.init = void 0;
 
-    var go = require("../../gojs/release/go.js");
+    var go = require("./gojs/release/go.js");
     function init() {
         var $ = go.GraphObject.make;
         var myDiagram = $(go.Diagram, 'myDiagramDiv',
@@ -56,12 +56,37 @@
                 )
             );
         myDiagram.model = new go.GraphLinksModel([
-            // Insert graph here
+            { key: '0', isGroup: true },
+            { key: 'group00node00', img: 'group00node00', group: '0' },
+            { key: 'group00node01', img: 'group00node01', group: '0' },
+            { key: 'group00node02', img: 'group00node02', group: '0' },
+            { key: 'group00node03', img: 'group00node03', group: '0' },
+            { key: 'group00node04', img: 'group00node04', group: '0' },
+            { key: 'group00node05', img: 'group00node05', group: '0' },
+            { key: '1', isGroup: true },
+            { key: 'group01node00', img: 'group01node00', group: '1' },
+            { key: 'group01node01', img: 'group01node01', group: '1' },
+            { key: 'group01node02', img: 'group01node02', group: '1' },
+            { key: 'group01node03', img: 'group01node03', group: '1' },
+            { key: 'group01node04', img: 'group01node04', group: '1' },
+            { key: 'group01node05', img: 'group01node05', group: '1' },
+        ], [
+            { from: 'group00node00', to: 'group00node01', img: 'link00' },
+            { from: 'group00node01', to: 'group00node02', img: 'link01' },
+            { from: 'group00node02', to: 'group00node03', img: 'link02' },
+            { from: 'group00node03', to: 'group00node04', img: 'link03' },
+            { from: 'group00node04', to: 'group01node00', img: 'link04' },
+            { from: 'group01node00', to: 'group01node01', img: 'link05' },
+            { from: 'group01node01', to: 'group01node02', img: 'link06' },
+            { from: 'group01node02', to: 'group01node03', img: 'link07' },
+            { from: 'group01node03', to: 'group01node04', img: 'link08' },
+            { from: 'group01node04', to: 'group01node05', img: 'link09' },
+            { from: 'group01node05', to: 'group00node05', img: 'link10' },
         ]);
     }
 
     function convertKeyImage(img) {
-        return "imgs/" + img + ".png";
+        return "imgs/formPhmRules/" + img + ".png";
     }
 
     exports.init = init;
